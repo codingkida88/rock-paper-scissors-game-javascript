6aa16838ebd0df5b3ff256e2321acd1c95e64ca2
dreamcoder88 <70472973+dreamcoder88@users.noreply.github.com>
2021-10-30 18:45:11 +0530

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,100 +1,147 @@
-/* styles.css */
-/* universal selector - Applies to whole document */
-*{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    background: #082c6c;
-    color: #fff;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-/* To center everything in game */
-.game{
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
-    align-items: center;
+
+section {
+  height: 100vh;
+  background-color: rgb(39, 41, 68);
+  font-family: sans-serif;
 }
-  
-/* Title of the game */
-.title{
-    position: absolute;
-    top: 0;
-    font-size: 4rem;
-    z-index: 2;
+
+.score {
+  color: rgb(224, 224, 224);
+  height: 20vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
-  
-/* Score Board */
-.score{
-    display: flex;
-    width: 30vw;
-    justify-content: space-evenly;
-    position: absolute;
-    top: 70px;
-    z-index: 1;
+.score h2 {
+  font-size: 30px;
 }
-  
-/* Score  */
-.p-count,.c-count{
-    text-align: center;
-    font-size: 1.5rem;
-    margin-top: 1rem;
+.score p {
+  text-align: center;
+  padding: 10px;
+  font-size: 25px;
 }
-  
-/* displaying three buttons in one line */
-.options{
-    display: flex;
-    width: 50vw;
-    justify-content: space-evenly;
-    margin-top: 2rem;
+
+.intro {
+  color: rgb(224, 224, 224);
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  transition: opacity 0.5s ease;
 }
-  
-/* Styling on alll three buttons */
-.rock, .paper, .scissor{
-    padding: 0.8rem;
-    width: 100px;
-    border-radius: 10px;
-    background: green;
-    outline: none;
-    border-color: green;
-    border: none;
-    cursor: pointer;
+
+.intro h1 {
+  font-size: 50px;
 }
-  
-.move{
-    font-size: 2rem;
-    font-weight: bold;
+.intro button,
+.match button {
+  width: 150px;
+  height: 50px;
+  background: none;
+  border: none;
+  color: rgb(224, 224, 224);
+  font-size: 20px;
+  background: rgb(45, 117, 96);
+  border-radius: 3px;
+  cursor: pointer;
 }
-  
-/* Reload button style */
-.reload {
-    display: none;
-    margin-top: 2rem;
-    padding: 1rem;
-    background: green;
-    outline: none;
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
+
+.match {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: opacity 0.5s ease 0.5s;
 }
-  
-.result{
-    margin-top: 20px;
-    font-size: 1.2rem;
+.winner {
+  color: rgb(224, 224, 224);
+  text-align: center;
+  font-size: 50px;
 }
-  
-/* Responsive Design */
-@media screen and (max-width: 612px)
-{  
-    .title{
-        text-align: center;
-    }
-    .score{
-        position: absolute;
-        top: 200px;
-        width: 100vw;
-    }
-    .options{
-        width: 100vw;
-    }
+
+.hands,
+.options {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.player-hand {
+  transform: rotateY(180deg);
+}
+
+div.fadeOut {
+  opacity: 0;
+  pointer-events: none;
+}
+
+div.fadeIn {
+  opacity: 1;
+  pointer-events: all;
+}
+
+@keyframes shakePlayer {
+  0% {
+    transform: rotateY(180deg) translateY(0px);
+  }
+  15% {
+    transform: rotateY(180deg) translateY(-50px);
+  }
+  25% {
+    transform: rotateY(180deg) translateY(0px);
+  }
+  35% {
+    transform: rotateY(180deg) translateY(-50px);
+  }
+  50% {
+    transform: rotateY(180deg) translateY(0px);
+  }
+  65% {
+    transform: rotateY(180deg) translateY(-50px);
+  }
+  75% {
+    transform: rotateY(180deg) translateY(0px);
+  }
+  85% {
+    transform: rotateY(180deg) translateY(-50px);
+  }
+  100% {
+    transform: rotateY(180deg) translateY(0px);
+  }
+}
+
+@keyframes shakeComputer {
+  0% {
+    transform: translateY(0px);
+  }
+  15% {
+    transform: translateY(-50px);
+  }
+  25% {
+    transform: translateY(0px);
+  }
+  35% {
+    transform: translateY(-50px);
+  }
+  50% {
+    transform: translateY(0px);
+  }
+  65% {
+    transform: translateY(-50px);
+  }
+  75% {
+    transform: translateY(0px);
+  }
+  85% {
+    transform: translateY(-50px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
